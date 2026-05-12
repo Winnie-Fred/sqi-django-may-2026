caafa64077ad7c6549496c10b6d9e55cfda92356
Winifred Igboama <winifredigboama@gmail.com>
2026-05-12 14:44:44 +0100

    add first projects to git

---
+++ first_project/second_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def contact_me(request):
+    return HttpResponse("Contact me: djangoinstructor@gmail.com")
