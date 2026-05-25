ce6a209c84a96227ad9b91243e55d3934afa0bf0
Winifred Igboama <winifredigboama@gmail.com>
2026-05-25 14:06:30 +0100

    add modelform

--- lms/library/urls.py
+++ lms/library/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('book-list/', views.book_list, name='book_list'),
     path('books/<int:book_pk>/', views.book_detail, name='book_detail'),
+    path('create/book/', views.create_book_model_form, name="create_book_model_form")
 ]
--- lms/library/views.py
+++ lms/library/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Book
+from .forms import BookForm
 
 # Create your views here.
 
@@ -16,3 +16,18 @@ def book_list(request):
 def book_detail(request, book_pk):
     book = get_object_or_404(Book, pk=book_pk)
     return render(request, "library/book_detail.html", {"book": book})
+
+
+def create_book_model_form(request):
+    form = BookForm()
+
+    if request.method == "POST":
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("library:book_list")
+        
+    context = {
+        "form": form
+    }
+    return render(request, "library/create-book-model-form.html", context)
